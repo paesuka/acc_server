34acab5e9e232c1b5c78e87501e5ef25c3bbc57d
Pascal <p.kaufmann.dev@gmail.com>
2016-05-31 10:11:45 +0800

    add systemenv in dev for mongodb uri

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -4,9 +4,9 @@
 // ==================================
 module.exports = {
 
-  // MongoDB connection options
+  // MongoDB connection options TODO use system env for user and passwort
   mongo: {
-    uri: 'mongodb://localhost/accserver-dev'
+    uri: 'mongodb://acc-user:pw@ds019633.mlab.com:19633/acc-db'
   },
 
   // Seed database on startup
